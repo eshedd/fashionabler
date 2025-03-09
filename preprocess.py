92b78f83e993b3c5d2abae33e243c06004b36670
E-Dawg <ethanhshedd@gmail.com>
2025-03-08 17:32:32 -0800

    Preprocessing images
    Removes background

---
+++ preprocess.py
@@ -0,0 +1,23 @@
+import os
+from rembg import remove, new_session
+
+
+session = new_session()
+
+# Batch process all images in a directory
+input_dir = "imgs/flatlay/"
+output_dir = "imgs/segmented/"
+
+for img_file in os.listdir(input_dir):
+    print(img_file)
+    input_path = os.path.join(input_dir, img_file)
+    output_path = os.path.join(output_dir, img_file)
+    with open(input_path, 'rb') as i:
+        with open(output_path, 'wb') as o:
+            input = i.read()
+            output = remove(  # Auto-removes background
+                data=input,
+                session=session,
+                bgcolor=(255, 255, 255, 255)
+            )
+            o.write(output)
